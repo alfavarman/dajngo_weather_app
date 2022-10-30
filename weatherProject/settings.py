46431b20d11f8cb9279bd6760f5596933a9dd769
CKS <communicate2charles@gmail.com>
2022-10-31 04:13:06 +0700

    main view, api request by post cityname

--- weatherProject/settings.py
+++ weatherProject/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "weatherApp.config.weatherApp",
+    "weatherApp.apps.weatherAppConfig",
 ]
 
 MIDDLEWARE = [
