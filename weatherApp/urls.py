b09a64ff2a1371668d0c1579aa418cd4bf69a8eb
CKS <communicate2charles@gmail.com>
2022-10-31 19:54:32 +0700

    ifs logic in WeatherAppView

--- weatherApp/urls.py
+++ weatherApp/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 
+
+
 urlpatterns = [
     path('', views.WeatherAppView.as_view(), name='index'),
+    path("map/", views.MarkersMapView.as_view()),
+
 ]
--- weatherApp/views.py
+++ weatherApp/views.py
@@ -10,37 +10,41 @@ from weatherApp.constants import URL_LOC, URL_CIT, UNITS_ID
 
 
 # TODO interactive map
+class MarkersMapView(TemplateView):
+    template_name = "map.html"
+
+
 # TODO validator
 # TODO source data from db and if not db -> request API
 class WeatherAppView(TemplateView):
     template_name = 'index.html'
 
     def get_context_data(self):
+
         city = self.request.GET.get('city')
-        if self.request.GET.get is None:
-            context = {}
-        else:
-            if city:
-                city = str(city).replace(" ", "%20")
-                urls = f'{URL_CIT}{city}{UNITS_ID}'
-            else:
-                lon = float(self.request.GET.get('lon'))
-                lat = float(self.request.GET.get('lat'))
-                urls = f'{URL_LOC}{lat}&lon={lon}{UNITS_ID}'
+        lon = float(self.request.GET.get('lon'))
+        # TODO improve if's statements pythonics maybe validator
+        if city:
+            city = str(city).replace(" ", "%20")
+            urls = f'{URL_CIT}{city}{UNITS_ID}'
+        elif lon:
+            lon = float(self.request.GET.get('lon'))
+            lat = float(self.request.GET.get('lat'))
+            urls = f'{URL_LOC}{lat}&lon={lon}{UNITS_ID}'
 
-            source = urllib.request.urlopen(f'{urls}').read()
+        source = urllib.request.urlopen(f'{urls}').read()
 
-            city_weather = json.loads(source)
+        city_weather = json.loads(source)
 
-            context = {
-                'country': city_weather['sys']['country'],
-                'city': city_weather['name'],
-                'lat': city_weather['coord']['lat'],
-                'lon': city_weather['coord']['lon'],
-                'temperature': city_weather['main']['temp'],
-                'humidity': city_weather['main']['humidity'],
-                'description': city_weather['weather'][0]['description'],
-                'icon': city_weather['weather'][0]['icon'],
-            }
+        context = {
+            'country': city_weather['sys']['country'],
+            'city': city_weather['name'],
+            'lat': city_weather['coord']['lat'],
+            'lon': city_weather['coord']['lon'],
+            'temperature': city_weather['main']['temp'],
+            'humidity': city_weather['main']['humidity'],
+            'description': city_weather['weather'][0]['description'],
+            'icon': city_weather['weather'][0]['icon'],
+        }
 
         return context
