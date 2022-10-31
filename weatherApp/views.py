ef98dccc618b8874c8b9cfa2d86a43c51051f86f
CKS <communicate2charles@gmail.com>
2022-10-31 19:27:24 +0700

    views from def to class

--- weatherApp/views.py
+++ weatherApp/views.py
@@ -2,49 +2,45 @@ import json
 import urllib.request
 import requests
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
+from django.views.generic.list import ListView
 
 from weatherApp.constants import URL_LOC, URL_CIT, UNITS_ID
 
 
-def index(request):
-    # TODO interactive map
+# TODO interactive map
+# TODO validator
+# TODO source data from db and if not db -> request API
+class WeatherAppView(TemplateView):
+    template_name = 'index.html'
 
-    if request.method == 'GET':
-        # TODO - get??
-        # TODO validator
-
-        city = request.GET.get('city')
-        if city:
-            city = str(city).replace(" ", "%20")
-            urls = f'{URL_CIT}{city}{UNITS_ID}'
+    def get_context_data(self):
+        city = self.request.GET.get('city')
+        if self.request.GET.get is None:
+            context = {}
         else:
-            lon = float(request.GET.get('lon'))
-            lat = float(request.GET.get('lat'))
-            urls = f'{URL_LOC}{lat}&lon={lon}{UNITS_ID}'
+            if city:
+                city = str(city).replace(" ", "%20")
+                urls = f'{URL_CIT}{city}{UNITS_ID}'
+            else:
+                lon = float(self.request.GET.get('lon'))
+                lat = float(self.request.GET.get('lat'))
+                urls = f'{URL_LOC}{lat}&lon={lon}{UNITS_ID}'
 
+            source = urllib.request.urlopen(f'{urls}').read()
 
+            city_weather = json.loads(source)
 
-        # TODO validator
-        # TODO source data from db and if not db -> request API
-        source = urllib.request.urlopen(f'{urls}').read()
+            context = {
+                'country': city_weather['sys']['country'],
+                'city': city_weather['name'],
+                'lat': city_weather['coord']['lat'],
+                'lon': city_weather['coord']['lon'],
+                'temperature': city_weather['main']['temp'],
+                'humidity': city_weather['main']['humidity'],
+                'description': city_weather['weather'][0]['description'],
+                'icon': city_weather['weather'][0]['icon'],
+            }
 
-
-        # json > dict
-        city_weather = json.loads(source)
-        #city_weather = requests.get(f"{urls}").json()
-        weather = {
-            'country': city_weather['sys']['country'],
-            'city': city_weather['name'],
-            'lat': city_weather['coord']['lat'],
-            'lon': city_weather['coord']['lon'],
-            'temperature': city_weather['main']['temp'],
-            'humidity': city_weather['main']['humidity'],
-            'description': city_weather['weather'][0]['description'],
-            'icon': city_weather['weather'][0]['icon']
-        }
-        context = weather
-
-    else:
-        context = {}
-
-    return render(request, 'weatherApp/index.html', context)
+        return context
